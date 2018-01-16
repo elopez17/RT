c890d69b6c06d913799e8cc3efc29606a3fdfac6
Eric Lopez <elopez@e1z3r1p12.42.us.org>
2018-01-15 21:25:43 -0800

    more error checking

--- src/parse3.c
+++ src/parse3.c
@@ -6,7 +6,7 @@
 /*   By: oabdalha <oabdalha@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/27 13:32:37 by oabdalha          #+#    #+#             */
-/*   Updated: 2018/01/14 20:17:46 by eLopez           ###   ########.fr       */
+/*   Updated: 2018/01/15 18:18:36 by elopez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,13 @@ void	getlight(t_rt *rt)
 	char	*line;
 	int		i;
 
-	i = -1;
-	while (get_next_line(rt->fd, &line) > 0 && i < rt->nlights)
+	i = 0;
+	while (get_next_line(rt->fd, &line) > 0)
 	{
 		if (rt->nlights == 0)
 		{
-			rt->nlights = ft_atoi(ft_strchr(line, '(') + 1);
+			if ((rt->nlights = ft_atoi(ft_strchr(line, '(') + 1)) <= 0)
+				rt_error(2);
 			rt->light = (t_vect*)malloc(sizeof(t_vect) * rt->nlights);
 			ft_strdel(&line);
 			continue ;
@@ -32,7 +33,10 @@ void	getlight(t_rt *rt)
 			ft_strdel(&line);
 			break ;
 		}
-		rt->light[++i] = getxyz(line);
+		if (i < rt->nlights)
+			rt->light[i++] = getxyz(line);
+		else
+			rt_error(2);
 		ft_strdel(&line);
 	}
 }
